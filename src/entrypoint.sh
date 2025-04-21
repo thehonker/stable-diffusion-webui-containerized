03b708823b32b79940a9d03616c96925888dd33c
Edwin Pers <pers.edwin@gmail.com>
2025-04-21 18:51:53 -0400

    shell globbing is evil

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -5,10 +5,10 @@ echo "entrypoint.sh start"
 if [ -n $FORCE_REINSTALL ]; then
   echo "Forcing reinstall"
   find "${HOME}/stable-diffusion-webui/" -o -prune -exec rm -rvf -- {} +
-  cp -rv /stable-diffusion-webui-base/* /stable-diffusion-webui-base/.* "${HOME}/stable-diffusion-webui/"
+  find /stable-diffusion-webui-base/ -exec cp -t "${HOME}/stable-diffusion-webui/" {} +
 elif [ ! -d "${HOME}/stable-diffusion-webui" ]; then
   echo "Copying stable-diffusion-webui repo to persistent directory"
-  cp -rv /stable-diffusion-webui-base/* /stable-diffusion-webui-base/.* "${HOME}/stable-diffusion-webui/"
+  find /stable-diffusion-webui-base/ -exec cp -t "${HOME}/stable-diffusion-webui/" {} + 
 else
   echo "Found existing stable-diffusion-webui directory, using that"
 fi
