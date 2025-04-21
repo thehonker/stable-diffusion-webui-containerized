532263e6a1e35650ceb5a2d2d1cbbd81fe1d1b6b
Edwin Pers <pers.edwin@gmail.com>
2025-04-21 18:58:32 -0400

    oof

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -5,10 +5,10 @@ echo "entrypoint.sh start"
 if [ -n $FORCE_REINSTALL ]; then
   echo "Forcing reinstall"
   find "${HOME}/stable-diffusion-webui/" -o -prune -exec rm -rvf -- {} +
-  find /stable-diffusion-webui-base/ -exec cp -t "${HOME}/stable-diffusion-webui/" {} +
-elif [ ! -d "${HOME}/stable-diffusion-webui" ]; then
+  find /stable-diffusion-webui-base/ -o -prune -exec cp -t "${HOME}/stable-diffusion-webui/" {} +
+elif [ ! -d "${HOME}/stable-diffusion-webui/.git" ]; then
   echo "Copying stable-diffusion-webui repo to persistent directory"
-  find /stable-diffusion-webui-base/ -exec cp -t "${HOME}/stable-diffusion-webui/" {} + 
+  find /stable-diffusion-webui-base/ -o -prune -exec cp -t "${HOME}/stable-diffusion-webui/" {} + 
 else
   echo "Found existing stable-diffusion-webui directory, using that"
 fi
