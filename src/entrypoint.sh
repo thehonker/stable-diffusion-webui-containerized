0ab52ac0cd04b4c566163ab834c44ea35222465b
Edwin Pers <pers.edwin@gmail.com>
2025-04-21 19:05:40 -0400

    i need to ask a robot how to do this lol

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,11 +4,11 @@ echo "entrypoint.sh start"
 
 if [ -n $FORCE_REINSTALL ]; then
   echo "Forcing reinstall"
-  find "${HOME}/stable-diffusion-webui/" -o -prune -exec rm -rvf -- {} +
-  find /stable-diffusion-webui-base/ -o -prune -exec cp -t "${HOME}/stable-diffusion-webui/" {} +
+  find "${HOME}/stable-diffusion-webui/" -exec rm -rvf -- {} +
+  find /stable-diffusion-webui-base/ -exec cp -t "${HOME}/stable-diffusion-webui/" {} +
 elif [ ! -d "${HOME}/stable-diffusion-webui/.git" ]; then
   echo "Copying stable-diffusion-webui repo to persistent directory"
-  find /stable-diffusion-webui-base/ -o -prune -exec cp -t "${HOME}/stable-diffusion-webui/" {} + 
+  find /stable-diffusion-webui-base/ -exec cp -t "${HOME}/stable-diffusion-webui/" {} +
 else
   echo "Found existing stable-diffusion-webui directory, using that"
 fi
