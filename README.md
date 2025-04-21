295c23424e9654e226faf7eb4d18616761030859
Edwin Pers <pers.edwin@gmail.com>
2025-04-21 18:34:59 -0400

    support persisting stable-diffusion-webui across restarts

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # stable-diffusion-webui-containerized
 AUTOMATIC1111/stable-diffusion-webui, containerized
 
+
+Mount a volume to `/home/sdwebui/stable-diffusion-webui/` to avoid reinstalling at every restart
+
 ```
 ghcr.io/thehonker/stable-diffusion-webui:(dev|master)-latest
 ```
---
+++ src/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "entrypoint.sh start"
+
+if [-n $FORCE_REINSTALL ]; then
+  echo "Forcing reinstall"
+  rm -rvf "${HOME}/stable-diffusion-webui"
+  cp -rv /stable-diffusion-webui-base "${HOME}/stable-diffusion-webui"
+elif [ ! -d "${HOME}/stable-diffusion-webui" ]; then
+  echo "Copying stable-diffusion-webui repo to persistent directory"
+  cp -rv /stable-diffusion-webui-base "${HOME}/stable-diffusion-webui"
+else
+  echo "Found existing stable-diffusion-webui directory, using that"
+fi
+
+exec "${HOME}/stable-diffusion-webui/webui.sh" "$@"
